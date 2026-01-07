2bd322c785395833528c80e8e7442aef831af187
Raj Kishore Jha <rajisha0202@gmail.com>
2026-01-07 13:47:13 +0530

    Add outputs for Azure resource group

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "resource_group_name" {
+  description = "Name of the Azure Resource Group created"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "resource_group_location" {
+  description = "Location of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.location
+}
