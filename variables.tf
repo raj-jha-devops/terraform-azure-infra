73941b0cbf34f5dc36a7ae89754cb2c036d7f258
Raj Kishore Jha <rajisha0202@gmail.com>
2026-01-07 13:51:03 +0530

    Add variables for resource group and location

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
 
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "rg-demo-devops"
+}
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+  default     = "East US"
+}
